6a5d3456669f1f5054900bf94e245fb3be2b1da6
Freedomisgood <1063052964@qq.com>
2018-11-07 17:29:42 +0800

    finish the function of reminding on Wechat

---
+++ Nyedu.py
@@ -0,0 +1,74 @@
+import requests
+from lxml import etree
+import time
+import re
+
+
+headers={
+'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36',
+}
+_time = time.strftime("%Y-%m-%d", time.localtime())
+
+
+def get_newest_info(secret_key):
+    html = requests.get('http://jwc.njupt.edu.cn/1594/list.htm',headers = headers)
+    html.encoding = 'utf-8'
+    #Requests库的自身编码为: r.encoding = ‘ISO-8859-1’
+    data = etree.HTML(html.text)
+    contentList = data.xpath('//*[@id="newslist"]/div/table/tr')
+    for content in contentList:
+        title = content.xpath('td/table/tr/td[1]/a/text()')[0]
+        href = content.xpath('td/table/tr/td[1]/a/@href')[0]
+        submittime = content.xpath('td/table/tr/td[2]/div/text()')[0]
+        if title.startswith('【实践科】') and submittime=="2018-11-06":
+            if href.endswith('.htm'):
+                detail = getDetailpage(href)
+                data_info = {
+                    'text': title ,
+                    'desp': detail
+                }
+                submit_info(secret_key,data_info)
+
+
+def getDetailpage(aurl):
+    prefix = 'http://jwc.njupt.edu.cn'
+    url = prefix + aurl
+    html = requests.get(url=url,headers=headers)
+    html.encoding = 'utf-8'
+    data = etree.HTML(html.text)
+    contentList = data.xpath('//div[@id="container_content"]/table/tr/td/table[5]/tr/td/div/div/div')[0]
+    str = ''
+    for content in contentList:
+        if content.tag == 'table':
+            tabletitle = content.xpath('tbody/tr[1]/td')  # 表格头
+            tabletitleList = map(lambda x: x.xpath('string(.)'), tabletitle)
+            tablehead = '|' + '|'.join(tabletitleList) + '|'  # 效果为|序号|学号|姓名|性别|学院|专业|
+            tableover = '|' + ':---:|' * len(tabletitle)  # 居中显示
+
+            tablecontent = ''
+            trs = content.xpath('tbody/tr')
+            for tr in trs[1:]:                                 #多行row
+                tds = map(lambda x: x.xpath('string(.)'), tr)  # 一行内容
+                tablecontent += '|' + '|'.join(tds) + '|' + '\n'
+
+            tableinfo = tablehead + '\n' + tableover + '\n' + tablecontent
+            str += '\n' + tableinfo + '\n' #将表格分离开一点
+        else:
+            Pcontent = content.xpath('string(.)')
+            if re.match('["一二三四五六七八九十"]+、', Pcontent):
+                str += "####" + Pcontent + '\n\n'
+            elif re.match('\d+.', Pcontent):
+                str += "#####" + Pcontent + '\n\n'
+            else:
+                str += content.xpath('string(.)') + '\n\n'
+    return str
+
+
+def submit_info(secret_key,info):
+    requests.post(url = 'https://sc.ftqq.com/{}.send'.format(secret_key),data=info)
+
+
+if __name__ == "__main__" :
+    get_newest_info('SCU35113Te369cebc21f6e483c03fffc400c4c5c05bdad63995c32')
+
+
--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # SeverChan_Nyedu
-Crawl the infomation about competitons, when new infos comes, remind people on Wechat by ServerChan
+Crawl the infomation about competitons, when new infos comes, remind people on Wechat by ServerChan:
+
+:heart_eyes:该程序通过爬取[南邮教务处](http://jwc.njupt.edu.cn/1594/list.htm),将当前时间与最新时间进行匹配,如果是当日则通过ServerChan发送到本人的微信,提醒有新的竞赛.
+
+
+
+
+
+#### 完成笔记:
+
+##### 1.关于`lxml`中`etree.xpath()`对于`tbody`的处理
+
+> 该网页通过`table`对页面进行分布设置,其中`table`标签会自动生成`tbody`标签,如图..此时用`xpath`进行匹配的时候就不需要将`tbody`加上,否则匹配不到
+
+布局`<Table>`
+
+![Nonetbody](https://github.com/Freedomisgood/SeverChan_Nyedu/Nonetbody.jpg)
+
+表格`<table>`
+
+![Table_tbody](https://github.com/Freedomisgood/SeverChan_Nyedu/Table_tbody.jpg)
+
+
+
+可以看到的是在Chrome调试助手里面,`<table>`下面都是会自动生成`<tbody>`标签的,而我们再通过**网页源码**看看..==>可以发现的是:
+
+**`<table>`布局是没有`<tbody>`的,只有表格才有**,所以这也是为什么用xpath()表格里必须加上`tbody`才能匹配,而`table`布局中不能加`tbody`的原因
+
+```python
+#布局获得内容
+for content in contentList:
+    title = content.xpath('td/table/tr/td[1]/a/text()')[0]
+    href = content.xpath('td/table/tr/td[1]/a/@href')[0]
+    submittime = content.xpath('td/table/tr/td[2]/div/text()')[0]
+
+#表格
+if content.tag == 'table':
+    tabletitle = content.xpath('tbody/tr[1]/td')  # 表格头
+    tabletitleList = map(lambda x: x.xpath('string(.)'), tabletitle)
+    tablehead = '|' + '|'.join(tabletitleList) + '|' 
+    tableover = '|' + ':---:|' * len(tabletitle)  # 居中显示
+```
+
+![real_code](https://github.com/Freedomisgood/SeverChan_Nyedu/real_code.jpg)
+
+
+
+##### 2.`requests.get()`获得的response的编码问题
+
+```python
+html = requests.get('http://jwc.njupt.edu.cn/1594/list.htm',headers = headers)
+html.encoding = 'utf-8'
+#Requests库的自身编码为: r.encoding = ‘ISO-8859-1’
+```
